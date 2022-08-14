68ab3a8499db0b383b54e8f737fe3b8a121d0229
tianjos <tbaguga@yahoo.com.br>
2022-08-13 21:16:59 -0300

    update linter config

--- tests/raffle.spec.ts
+++ tests/raffle.spec.ts
@@ -1,9 +1,8 @@
-import test from "ava";
+import test from 'ava';
 import { Creator } from '../src/creator';
 import { Raffle } from '../src/raffle';
 
-test('raffle creation', t => {
-    const raffle = new Raffle(new Creator('foo'), 10, 100, 30);
-    
-    t.true(raffle instanceof Raffle)
-})
+test('raffle creation', (t) => {
+  const raffle = new Raffle(new Creator('foo'), 10, 100, 30);
+  t.true(raffle instanceof Raffle);
+});
